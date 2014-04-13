a41a829df81143bc146566d36b3cbc7a461b9a4a
Brian 'Redbeard' Harrington <redbeard@dead-city.org>
2014-04-13 12:39:18 -0700

    Supporting Fedora in linuxdist
    Fedora uses different library names for things like boost. By being
    able to detect Fedora vs Red Hat this will broaden support.

--- src/osfinder.sh
+++ src/osfinder.sh
@@ -2,6 +2,10 @@
 
 if [ -f "/etc/redhat-release" ]; then
 	linuxdist="redhat"
+    flavor=$(awk '{print $1}' /etc/redhat-release)
+    if [ "$flavor" == "Fedora" ]; then
+        linuxdist="fedora"
+    fi
 elif [ -f "/etc/debian_version" ]; then
 	linuxdist="debian"
 else
